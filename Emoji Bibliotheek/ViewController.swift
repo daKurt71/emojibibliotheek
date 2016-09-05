90090d99cb3286d0aaffafa97f14e7412e139428
Kurt Peeters <dakurt71@gmail.com>
2016-09-05 10:06:40 +0200

    Initial Commit

---
+++ Emoji Bibliotheek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Bibliotheek
+//
+//  Created by Kurt Peeters on 5/09/16.
+//  Copyright © 2016 Kurt Peeters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
